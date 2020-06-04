7a0cb8fa60645e01223344c07712665cc6da07f1
vidura <vidura.hemawansa@pyxle.io>
2020-06-04 09:00:11 +0530

    added files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProviderComponent } from './provider/provider.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    ProviderComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'provider', component: ProviderComponent },
+      { path: 'services', component: ServicesComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
